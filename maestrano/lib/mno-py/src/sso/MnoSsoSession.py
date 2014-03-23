1f70add0720044b06448419d95b1e937bf7bb53c
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-03-24 04:16:08 +1100

    fixed mno api

--- maestrano/lib/mno-py/src/sso/MnoSsoSession.py
+++ maestrano/lib/mno-py/src/sso/MnoSsoSession.py
@@ -1,3 +1,8 @@
+import pytz
+import urllib2
+import json
+from datetime import datetime,timedelta
+import dateutil.parser
 
 # Helper class used to check the validity
 # of a Maestrano session
@@ -22,15 +27,19 @@ class MnoSsoSession(object, ):
     def __init__(self, mno_settings, session):
         self.settings = mno_settings
         self.session = session
-        self.uid = session['mno_uid']
-        self.token = session['mno_session']
-        self.recheck = DateTime(session['mno_session_recheck'])
+        self.uid = None
+        self.token = None
+        self.recheck = None
+        
+        if 'mno_uid' in session: self.uid = session['mno_uid']
+        if 'mno_session' in session: self.token = session['mno_session']
+        if 'mno_session_recheck' in session: self.recheck = dateutil.parser.parse(session['mno_session_recheck'])
 
     # Check whether we need to remotely check the
     # session or not
     def remoteCheckRequired(self):
-        if ((self.uid and self.token) and self.recheck):
-            if (self.recheck > DateTime('NOW')):
+        if (self.uid is not None and self.token is not None and isinstance(self.recheck, datetime)):
+            if ((self.recheck - datetime.now(pytz.utc)) > timedelta(seconds = 0)):
                 return False
         return True
     
@@ -42,15 +51,15 @@ class MnoSsoSession(object, ):
 
     # Fetch url and return content. Wrapper function.
     def fetchUrl(self, url):
-        return file_get_contents(url)
+        return urllib2.urlopen(url).read()
 
     # Perform remote session check on Maestrano
     def performRemoteCheck(self):
-        json = self.fetchUrl(self.sessionCheckUrl())
-        if json:
-            response = json_decode(json, True)
+        json_response = self.fetchUrl(self.sessionCheckUrl())
+        if json_response:
+            response = json.loads(json_response)
             if (response['valid'] and response['recheck']):
-                self.recheck = DateTime(response['recheck'])
+                self.recheck = dateutil.parser.parse(response['recheck'])
                 return True
         return False
 
@@ -62,7 +71,7 @@ class MnoSsoSession(object, ):
     def isValid(self):
         if self.remoteCheckRequired():
             if self.performRemoteCheck():
-                self.session['mno_session_recheck'] = self.recheck.format(DateTime.ISO8601)
+                self.session['mno_session_recheck'] = self.recheck.isoformat()
                 return True
             else:
                 return False
